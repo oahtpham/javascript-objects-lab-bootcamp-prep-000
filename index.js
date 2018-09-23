ef51ee53c7911215551340d250efc98d1eca81d1
phamthaop.9 <phamthaop.9@gmail.com>
2018-09-23 19:03:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function updateObjectWithKeyAndValue (object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-object[key] = value;
-  return object;
+  recipes.prop2 = 2;
+  return recipes;
 }
 
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop2', 2)
@@ -25,3 +25,5 @@ function destructivelyDeleteFromObjectByKey (object, key) {
   delete recipes.prop
   return recipes
 }
+
+destructivelyDeleteFromObjectByKey (recipes, 'prop')
