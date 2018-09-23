dabf5907d51727982a1a5037194344f8d5c8bbc6
phamthaop.9 <phamthaop.9@gmail.com>
2018-09-23 19:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = { prop: 1 }
 
 function updateObjectWithKeyAndValue (object, key, value) {
-  Object.assign({}, object, { key: value });
+  Object.assign({}, obj, { key: value });
 }
 
 updateObjectWithKeyAndValue(recipes, 'prop2', 2)
